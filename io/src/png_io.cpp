fda122c6e635b5714a941e0dc5624989d9b6e3de
Savant Krishna <savant.2020@gmail.com>
2014-02-26 06:02:01 +0530

    Save histogram from offline rendering

--- io/src/png_io.cpp
+++ io/src/png_io.cpp
@@ -42,6 +42,19 @@
 #include <vtkPNGWriter.h>
 #include <vtkSmartPointer.h>
 #include <vtkImageFlip.h>
+#include <vtkDoubleArray.h>
+#include <vtkRenderer.h>
+#include <vtkXYPlotActor.h>
+#include <vtkRenderWindow.h>
+#include <vtkTextProperty.h>
+#include <vtkFieldData.h>
+#include <vtkDataObject.h>
+#include <vtkWindowToImageFilter.h>
+#include <vtkDoubleArray.h>
+#include <vtkProperty2D.h>
+#include <vtkRendererSource.h>
+#include <vtkGraphicsFactory.h>
+#include <vtkImagingFactory.h>
 
 namespace 
 {
@@ -57,8 +70,90 @@ namespace
     writer->SetInputConnection(flipYFilter->GetOutputPort());
     writer->Write();
   }
-};
 
+  void renderAndWritePng(cosnt std::string& file_name, vtkSmartPointer<vtkXYPlotActor>& xy_plot, int width, int height)
+  {
+    // Setup offscreen rendering
+    vtkSmartPointer<vtkGraphicsFactory> graphics_factory = vtkSmartPointer<vtkGraphicsFactory>::New();
+    graphics_factory->SetOffScreenOnlyMode(1);
+    graphics_factory->SetUseMesaClasses(1);
+
+    vtkSmartPointer<vtkImagingFactory> imaging_factory = vtkSmartPointer<vtkImagingFactory>::New();
+    imaging_factory->SetUseMesaClasses(1); 
+
+    vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+    renderer->AddActor2D(xy_plot);
+    renderer->SetBackground(1,1,1); // Background color white
+
+    vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
+    renderWindow->AddRenderer(renderer);
+    renderWindow->SetSize(width, height);
+    renderWindow->SetBorders(1)
+    renderWindow->SetOffScreenRendering(1);
+    renderWindow->Render();
+
+    vtkSmartPointer<vtkWindowToImageFilter> windowToImageFilter = vtkSmartPointer<vtkWindowToImageFilter>::New();
+    windowToImageFilter->SetInput(renderWindow);
+    windowToImageFilter->SetInputBufferTypeToRGBA(); //also record the alpha (transparency) channel
+    windowToImageFilter->Update();
+
+    vtkSmartPointer<vtkPNGWriter> writer = vtkSmartPointer<vtkPNGWriter>::New();
+    writer->SetFileName(file_name.c_str());
+    writer->SetInputConnection(windowToImageFilter->GetOutputPort());
+    writer->Write();
+  }
+
+  void saveHistPNGFile (const std::string& file_name, vtkSmartPointer<vtkDoubleArray>& xy_array, int width, int height)
+  {
+    vtkSmartPointer<vtkXYPlotActor> xy_plot = vtkSmartPointer<vtkXYPlotActor>::New();
+    xy_plot->SetDataObjectPlotModeToColumns();
+    xy_plot->SetXValuesToValue();
+
+    vtkSmartPointer<vtkFieldData> field_values = vtkSmartPointer<vtkFieldData>::New();
+    field_values->AddArray(xy_array);
+
+    vtkSmartPointer<vtkDataObject> field_data = vtkSmartPointer<vtkDataObject>::New();
+    field_data->SetFieldData(field_values);
+
+    xy_plot->AddDataObjectInput(field_data);
+    xy_plot->SetPlotColor(0, 1.0, 0.0, 0.0);
+
+    xy_plot->SetDataObjectXComponent(0,0);
+    xy_plot->SetDataObjectYComponent(0,1);
+    xy_plot->PlotPointsOn();
+    xy_plot->PlotCurveLinesOn();
+
+    double min_max[2];
+    xy_array->GetRange(min_max, 1);
+
+    xy_plot->SetYTitle (""); xy_plot->SetXTitle ("");
+    xy_plot->SetYRange (min_max[0], min_max[1]); 
+    xy_plot->SetXRange (0, static_cast<double> (xy_array->GetNumberOfTuples () - 1));
+
+    //renwinint.xy_plot_->SetTitle (id.c_str ());
+    xy_plot->GetProperty ()->SetColor (0, 0, 0);
+
+    // Adjust text properties
+    vtkSmartPointer<vtkTextProperty> tprop = xy_plot->GetTitleTextProperty ();
+    xy_plot->AdjustTitlePositionOn ();
+    tprop->SetFontSize (8);
+    tprop->ShadowOff (); tprop->ItalicOff ();
+    tprop->SetColor (xy_plot->GetProperty ()->GetColor ());
+
+    xy_plot->SetAxisLabelTextProperty (tprop);
+    xy_plot->SetAxisTitleTextProperty (tprop);
+    xy_plot->SetNumberOfXLabels (8);
+    xy_plot->GetProperty ()->SetPointSize (3);
+    xy_plot->GetProperty ()->SetLineWidth (2);
+
+    xy_plot->SetPosition (0, 0);
+    xy_plot->SetWidth (1); xy_plot->SetHeight (1);
+
+    renderAndWritePng(file_name, xy_plot, width, height);
+
+  }
+
+};
 
 
 
@@ -144,3 +239,60 @@ pcl::io::savePNGFile (const std::string& file_name, const pcl::PointCloud<pcl::P
 	saveShortPNGFile(file_name, &data[0], cloud.width, cloud.height,1);
 }
 
+template <typename PointT> void
+pcl::io::savePNGFile (const std::string& file_name, const pcl::PointCloud<PointT>& cloud, const std::string& field_name, const int index, int width, int height)
+{
+  if (index < 0 || index >= cloud.points.size ())
+  {
+    PCL_ERROR ("[pcl::io::savePNGFile] Invalid point index (%d) given!\n", index);
+    return;
+  }
+
+  std::vector<pcl::PCLPointField> fields;
+  // Check if our field exists
+  int field_idx = pcl::getFieldIndex<PointT> (cloud, field_name, fields);
+  if (field_idx == -1)
+  {
+    PCL_ERROR ("[pcl::io::savePNGFile] The specified field <%s> does not exist!\n", field_name.c_str ());
+    return;
+  }
+
+  vtkSmartPointer<vtkDoubleArray> xy_array = vtkSmartPointer<vtkDoubleArray>::New ();
+  xy_array->SetNumberOfComponents (2);
+  xy_array->SetNumberOfTuples (fields[field_idx].count);
+
+  // Parse the cloud data and store it in the array
+  double xy[2];
+  for (uint32_t d = 0; d < fields[field_idx].count; ++d)
+  {
+    xy[0] = d;
+    //xy[1] = cloud.points[index].histogram[d];
+    float data;
+    memcpy (&data, reinterpret_cast<const char*> (&cloud.points[index]) + fields[field_idx].offset + d * sizeof (float), sizeof (float));
+    xy[1] = data;
+    xy_array->SetTuple (d, xy);
+  }
+
+  saveHistPNGFile(file_name, xy_array, width, height);
+
+}
+
+template <typename PointT> void
+pcl::io::savePNGFile (const std::string& file_name, const pcl::PointCloud<PointT> &cloud, int hsize, int width, int height )
+{
+  vtkSmartPointer<vtkDoubleArray> xy_array = vtkSmartPointer<vtkDoubleArray>::New ();
+  xy_array->SetNumberOfComponents (2);
+  xy_array->SetNumberOfTuples (hsize);
+
+  // Parse the cloud data and store it in the array
+  double xy[2];
+  for (int d = 0; d < hsize; ++d)
+  {
+    xy[0] = d;
+    xy[1] = cloud.points[0].histogram[d];
+    xy_array->SetTuple (d, xy);
+  }
+
+  saveHistPNGFile(file_name, xy_array, width, height);
+
+}
